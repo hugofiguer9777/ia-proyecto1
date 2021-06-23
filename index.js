9b6719dd89701a44b2f96b1425b9960490b47a58
hugofiguer9777 <58827958+hugofiguer9777@users.noreply.github.com>
2021-06-22 23:14:56 -0600

    agregando minmax

--- index.js
+++ index.js
@@ -21,3 +21,60 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(` Running on port :${port}`)
 });
+
+function iniciar(tablero, jug) {
+
+  let valor = minimax(tablero, 0, true, 0);
+  let cad = getY(valor[1]) + '' + getX(valor[1]);
+  console.log("RESULTADO (", valor, getY(valor[1]), ',', getX(valor[1]), ')');
+
+  return cad;
+
+}
+
+function minimax(tablero, depth, isMaximizing, indice) {
+
+  if (depth == 3) {
+
+    return [heuristicas[indice], indice];
+  }
+  if (isMaximizing) {
+    let best = [-999, 0]
+
+    let tempMovs = allPosibleMovements(tablero, jugador);
+    let indexBest = [0, 0];
+
+    if (tempMovs.length == 0) { return [heuristicas[indice], indice]; }
+
+    for (var item of tempMovs) {
+
+      if (depth == 0 && heuristicas[item[1]] == 120) { return [heuristicas[item[1]], item[1]]; }
+
+      let tempTablero = fillingMovs(tablero, item, jugador);
+
+      let valor = minimax(tempTablero, depth + 1, false, item[1]);
+      if (valor[0] > best[0]) indexBest = item;
+      best = valor[0] > best[0] ? valor : best;
+
+      if (depth == 0) { console.log("mov (origen,destino,direccion):", item, "heuristica ", valor[0]); }
+    }
+
+    if (depth == 0) {
+      best[1] = indexBest[1];
+    }
+
+    return best;
+
+  } else {
+    let best = [999, 0];
+    let tempMovs = allPosibleMovements(tablero, oponente);
+    if (tempMovs.length == 0) { return [heuristicas[indice], indice]; }
+    for (var item of tempMovs) {
+      let tempTablero = fillingMovs(tablero, item, oponente);
+      let valor = minimax(tempTablero, depth + 1, true, item[1]);
+      best = valor[0] < best[0] ? valor : best;
+    }
+
+    return best;
+  }
+}
