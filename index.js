412845475c9188dc51479a7f70f7190299b1e2bf
hugofiguer9777 <58827958+hugofiguer9777@users.noreply.github.com>
2021-06-23 12:17:10 -0600

    movimientos

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function minimax(tablero, depth, isMaximizing, indice) {
   if (isMaximizing) {
     let best = [-999, 0]
 
-    let tempMovs = allPosibleMovements(tablero, jugador);
+    let tempMovs = posiblesMovimientos(tablero, jugador);
     let indexBest = [0, 0];
 
     if (tempMovs.length == 0) { return [heuristicas[indice], indice]; }
@@ -50,7 +50,7 @@ function minimax(tablero, depth, isMaximizing, indice) {
 
       if (depth == 0 && heuristicas[item[1]] == 120) { return [heuristicas[item[1]], item[1]]; }
 
-      let tempTablero = fillingMovs(tablero, item, jugador);
+      let tempTablero = llenandoMovimientos(tablero, item, jugador);
 
       let valor = minimax(tempTablero, depth + 1, false, item[1]);
       if (valor[0] > best[0]) indexBest = item;
@@ -67,14 +67,92 @@ function minimax(tablero, depth, isMaximizing, indice) {
 
   } else {
     let best = [999, 0];
-    let tempMovs = allPosibleMovements(tablero, oponente);
+    let tempMovs = posiblesMovimientos(tablero, oponente);
     if (tempMovs.length == 0) { return [heuristicas[indice], indice]; }
     for (var item of tempMovs) {
-      let tempTablero = fillingMovs(tablero, item, oponente);
+      let tempTablero = llenandoMovimientos(tablero, item, oponente);
       let valor = minimax(tempTablero, depth + 1, true, item[1]);
       best = valor[0] < best[0] ? valor : best;
     }
 
     return best;
   }
+}
+
+function posiblesMovimientos(tablero, jug) {
+  let movimientos = [];
+  for (var i = 0; i < tablero.length; i++) {
+    if (tablero[i] == jug) {
+      movimientos = movimientos.concat(getPosiblesMovimientos(tablero, i, jug));
+
+    }
+  }
+  return movimientos;
+}
+
+function getPosiblesMovimientos(tablero, indice, jug) {
+  let tempIndex = 0;
+  let step = [-1, -9, -8, -7, 1, 9, 8, 7];
+  let movimientos = [];
+
+  let maxX = getX(indice);
+  let maxY = getY(indice);
+  let indexX = 0;
+  let indexY = 0;
+  let enemigo = false;
+  let maxMovs = 0;
+
+  for (var i = 0; i < step.length; i++) {
+    enemigo = false;
+    tempIndex = indice;
+    if (step[i] == -1) maxMovs = maxX;
+    else if (step[i] == -9) maxMovs = Math.min(maxX, maxY);
+    else if (step[i] == -8) maxMovs = maxY;
+    else if (step[i] == -7) maxMovs = Math.min(7 - maxX, maxY);
+    else if (step[i] == 1) maxMovs = 7 - maxX;
+    else if (step[i] == 9) maxMovs = Math.min((7 - maxX), (7 - maxY));
+    else if (step[i] == 8) maxMovs = 7 - maxY;
+    else if (step[i] == 7) maxMovs = Math.min(maxX, 7 - maxY);
+
+    for (var j = 0; j < maxMovs; j++) {
+      tempIndex += step[i];
+
+      if (tempIndex >= 0 && tempIndex <= 64) {
+        if (tempIndex == 0 && enemigo && tablero[tempIndex] == 2) console.log('------------> ', indice, heuristicas[tempIndex]);
+
+        if (tablero[tempIndex] == jug) {
+          break;
+        } else if (tablero[tempIndex] == 2 && !enemigo) {
+          break;
+        } else if (tablero[tempIndex] == 2 && enemigo) {
+          movimientos.push([indice, tempIndex, step[i]]);
+          break;
+        } else if (tablero[tempIndex] != jug) {
+          enemigo = true;
+        }
+      } else {
+        break;
+      }
+      indexX++;
+      indexY++;
+    }
+  }
+
+  return movimientos;
+}
+
+function llenandoMovimientos(tablero, arr, jug) {
+  let newTablero = Object.assign([], tablero);
+  tempIndex = arr[0];
+
+  for (var i = 0; i < 8; i++) {
+    tempIndex += arr[2];
+    if (arr[2] > 0 && tempIndex <= arr[1]
+      || arr[2] < 0 && tempIndex >= arr[1]) {
+      newTablero[tempIndex] = jug + '';
+    } else {
+      break;
+    }
+  }
+  return newTablero;
 }
