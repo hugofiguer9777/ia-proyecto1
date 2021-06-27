07bd5ff6392e9eb940e083cb6c967dc2e9796095
hugofiguer9777 <58827958+hugofiguer9777@users.noreply.github.com>
2021-06-26 23:50:35 -0600

    agragando cambios y metodos get

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.get('/', (req, res) => {
+  turno = req.query.turno;
+  estado = req.query.estado;
+  console.log(turno, estado);
+  jugador = turno;
+  oponente = jugador == 1 ? 0 : 1;
+  cadTablero = estado;
+  convertCadToArray();
+  printTablero(board);
+  let resultado = iniciar(board, jugador);
+  console.log(jugador, "oponente: ", oponente);
+
+  res.send(resultado)
+})
+
+app.listen(port, () => {
+  console.log(` Running on port :${port}`)
+});
 
 var reversi = {
 
