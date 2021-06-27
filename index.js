3a0b0a84162fd81a48de79f60724271d4b45540d
hugofiguer9777 <58827958+hugofiguer9777@users.noreply.github.com>
2021-06-26 23:51:10 -0600

    variables sin utilidad

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
   turno = req.query.turno;
   estado = req.query.estado;
-  console.log(turno, estado);
-  jugador = turno;
-  oponente = jugador == 1 ? 0 : 1;
-  cadTablero = estado;
-  convertCadToArray();
-  printTablero(board);
   let resultado = iniciar(board, jugador);
   console.log(jugador, "oponente: ", oponente);
 
